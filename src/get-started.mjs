875bfb8e06ee59e650b13be58c7379c2c5693821
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-03-11 00:15:22 -0300

    feat: create a tensorflow project :)

---
+++ src/get-started.mjs
@@ -0,0 +1,25 @@
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-node";
+
+async function main() {
+  const model = tf.sequential();
+
+  model.add(
+    tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
+  );
+  model.add(tf.layers.dense({ units: 1, activation: "linear" }));
+  model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
+
+  const xs = tf.randomNormal([100, 10]);
+  const ys = tf.randomNormal([100, 1]);
+
+  model.fit(xs, ys, {
+    epochs: 100,
+    callbacks: {
+      onEpochEnd: (epoch, log) =>
+        console.log(`Epoch ${epoch}: loss = ${log.loss}`),
+    },
+  });
+}
+
+main();
